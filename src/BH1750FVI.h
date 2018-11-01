9102940fb297c8993822faff9a4fa34afdf0657f
PeterEmbedded <PeterEmbedded@users.noreply.github.com>
2018-11-01 10:32:01 +0100

    Update BH1750FVI.h
    Added a new constructor to omit the address pin

--- src/BH1750FVI.h
+++ src/BH1750FVI.h
@@ -41,25 +41,32 @@ class BH1750FVI {
   public:
 
     typedef enum eDeviceAddress {
-        k_DevAddress_L = 0x23,
-  	k_DevAddress_H = 0x5C
+      k_DevAddress_L = 0x23,
+      k_DevAddress_H = 0x5C
     } eDeviceAddress_t;
 
     typedef enum eDeviceMode {
-        k_DevModeContHighRes     = 0x10,
-  	k_DevModeContHighRes2    = 0x11,
-  	k_DevModeContLowRes      = 0x13,
-  	k_DevModeOneTimeHighRes  = 0x20,
-  	k_DevModeOneTimeHighRes2 = 0x21,
-  	k_DevModeOneTimeLowRes   = 0x23
+      k_DevModeContHighRes     = 0x10,
+      k_DevModeContHighRes2    = 0x11,
+      k_DevModeContLowRes      = 0x13,
+      k_DevModeOneTimeHighRes  = 0x20,
+      k_DevModeOneTimeHighRes2 = 0x21,
+      k_DevModeOneTimeLowRes   = 0x23
     } eDeviceMode_t;
 
     typedef enum eDeviceState {
-        k_DevStatePowerDown = 0x00,
-  	k_DevStatePowerUp   = 0x01,
-  	k_DevStateReset     = 0x07
+      k_DevStatePowerDown = 0x00,
+      k_DevStatePowerUp   = 0x01,
+      k_DevStateReset     = 0x07
     } eDeviceState_t;
-	
+    
+    /*
+    * Constructor
+    * Omits the need for an extra addresspin and sets the address to Low
+    * @param DeviceMode, the mode to set the device in
+    */
+    BH1750FVI(eDeviceMode_t DeviceMode);
+    
     /*
     * Constructor
     * @param AddressPin, pin number the device is attached to
@@ -96,27 +103,25 @@ class BH1750FVI {
      */
     void Reset(void);
 
- 	  
+       
   private:
     
-      /*
-       * Set the High or Low I2C address
-       * @param DeviceAddress, the high or low address
-       */
-      void SetAddress(eDeviceAddress_t DeviceAddress);
+    /*
+     * Set the High or Low I2C address
+     * @param DeviceAddress, the high or low address
+     */
+    void SetAddress(eDeviceAddress_t DeviceAddress);
 
-      /*
-       * Write data to the I2C port
-       * @param Data, data to write
-       */
-      void I2CWrite(uint8_t Data);  
+    /*
+     * Write data to the I2C port
+     * @param Data, data to write
+     */
+    void I2CWrite(uint8_t Data);  
 
-      uint8_t m_AddressPin;               //!< Addresspin number the device is connected to
-      eDeviceAddress_t m_DeviceAddress;   //!< I2C address of the device
-      eDeviceMode_t m_DeviceMode;         //!< Mode of the device
+    uint8_t m_AddressPin;               //!< Addresspin number the device is connected to
+    eDeviceAddress_t m_DeviceAddress;   //!< I2C address of the device
+    eDeviceMode_t m_DeviceMode;         //!< Mode of the device
+    bool m_AddressPinUsed;              //!< Constructed with or without the address pin
 };
 
 #endif
-
-
-
